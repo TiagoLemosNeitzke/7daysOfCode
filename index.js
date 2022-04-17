c6f3b85fb0302c3f81fcdeb097bade8038661b11
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2022-04-17 09:28:58 -0400

    apagando espaços em branco

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const menuEl = document.getElementById('lateral--menu')
 
 function mostraMenu() {
 
-
     if (menuEl.classList == 'active') {
         menuEl.classList.remove('active')
     } else {
         menuEl.classList.add('active')
     }
-
-
 }
